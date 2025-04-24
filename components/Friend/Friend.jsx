68f2c93172be97954ac4f47812463503dec54866
om salunke <omsaunke343@gmail.com>
2025-04-24 09:42:46 +0530

    displaying friend list

--- components/Friend/Friend.jsx
+++ components/Friend/Friend.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
-
+import React, { useContext, useEffect, useState } from "react";
+import Link from "next/link";
+import images from "../../assets/index";
+import Card from "./Card/Card";
+import Chat from "./Chat/Chat";
+import { ChatAppContext } from "@/Context/ChatAppContext";
+import Style from "./Friend.module.css";
 const Friend = () => {
+  const arr = [1, 3, 34, 5, 6];
+  const {
+    sendMessage,
+    account,
+    friendLists,
+    userName,
+    loading,
+    readUser,
+    readMessage,
+    friendMsg,
+    currentUserAddress,
+    currentUserName,
+  } = useContext(ChatAppContext);
+  console.log(friendLists);
   return (
-    <div>
-      
+    <div className={Style.Friend}>
+      <div className={Style.Friend_box}>
+        <div className={Style.Friend_box_left}>
+          {friendLists.map((el, i) => {
+            return (
+              <>
+                <Card key={i + 1} i={i} el={el} readMessage={readMessage} readUser={readUser} />
+              </>
+            );
+          })}
+        </div>
+        <div className={Style.Friend_box_right}>
+          <Chat functionName={sendMessage} readMessage={readMessage} friendMsg={friendMsg} account={account} userName={userName} currentUserName={currentUserName} loading={loading} currentUserAddress={currentUserAddress} />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Friend
+export default Friend;
--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ const ChatApp = () => {
   return (
     <div>
       <Filter/>
+      <Friend/>
     </div>
   )
 }
